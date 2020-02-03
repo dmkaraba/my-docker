71a6007a75a9a792f1b8828478ac96403bcc7db3
Dzmitry Karaba <dmkaraba@gmail.com>
2020-02-03 17:51:39 +0300

    www

--- launch_dockers.py
+++ launch_dockers.py
@@ -1,17 +1,29 @@
 import os
 import docker
-import subprocess
 
 
 client = docker.from_env()
 
 def run_containers(client):
-    redis_container = client.containers.run(image='redis', name='my-redis-cnt', detach=True)
+    redis_container = client.containers.run(
+        image='redis',
+        name='redis-cnt',
+        detach=True,
+    )
     publisher_container = client.containers.run(
-        image='publisher', name='pub-app', links={'my-redis-cnt': 'redis'}, ports={'5000/tcp': '5000'}, detach=True)
+        image='publisher',
+        name='pub-app',
+        links={'redis-cnt': 'redis'},
+        ports={'5000/tcp': '5000'},
+        detach=True,
+    )
     subscriber_container = client.containers.run(
-        image='subscriber', name='sub-app', links={'my-redis-cnt': 'redis'},
-        volumes={'D:/dev/my-docker/redis_sub_docker/logs/': {'bind': '/logs/', 'mode': 'rw'}}, detach=True)
+        image='subscriber',
+        name='sub-app',
+        links={'redis-cnt': 'redis'},
+        volumes={'D:/dev/my-docker/subscriber/': {'bind': '/var/log/', 'mode': 'rw'}},
+        detach=True,
+    )
     return redis_container.id, publisher_container.id, subscriber_container.id
 
 
@@ -19,4 +31,3 @@ if __name__ == '__main__':
     cnt_ids = run_containers(client)
     print(cnt_ids)
     os.system(f'async_watchdog.py {" ".join(cnt_ids)}')
-
