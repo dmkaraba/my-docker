c9316d64c063e54757bed1b3d46134b9c45349e9
Dzmitry Karaba <dmkaraba@gmail.com>
2020-02-04 17:30:56 +0300

    wip

--- launch_dockers.py
+++ launch_dockers.py
@@ -4,21 +4,21 @@ import docker
 
 client = docker.from_env()
 
-def run_containers(client):
+def run_containers(client, redis_name, subscriber_name, publisher_name):
     redis_container = client.containers.run(
-        image='redis',
+        image=redis_name,
         name='redis-cnt',
-        detach=True,
+        detach=True
     )
     publisher_container = client.containers.run(
-        image='publisher',
+        image=publisher_name,
         name='pub-app',
         links={'redis-cnt': 'redis'},
         ports={'5000/tcp': '5000'},
         detach=True,
     )
     subscriber_container = client.containers.run(
-        image='subscriber',
+        image=subscriber_name,
         name='sub-app',
         links={'redis-cnt': 'redis'},
         volumes={'D:/dev/my-docker/subscriber/': {'bind': '/var/log/', 'mode': 'rw'}},
@@ -28,6 +28,10 @@ def run_containers(client):
 
 
 if __name__ == '__main__':
-    cnt_ids = run_containers(client)
-    print(cnt_ids)
-    os.system(f'async_watchdog.py {" ".join(cnt_ids)}')
+    subscriber_image, _ = client.images.build(path='./subscriber', tag='subscriber:test')
+    print(subscriber_image, 'build DONE')
+    publisher_image,  _ = client.images.build(path='./publisher', tag='publisher:test')
+    print(publisher_image, 'build DONE')
+    cnt_ids = run_containers(client, 'redis:latest', subscriber_image.tags[0], publisher_image.tags[0])
+    print(cnt_ids, 'run DONE')
+    # os.system(f'async_watchdog.py {" ".join(cnt_ids)}')
